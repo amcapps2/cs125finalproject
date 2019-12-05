81ef3437fefde9823896fb13d20ddae10cb6a0ba
amcapps2 <amcapps2@illinois.edu>
2019-12-04 22:15:28 -0600

    updated icon

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.cs125finalproject;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,15 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         newGameButton = (Button) findViewById(R.id.newGameButton);
-        //starts newGame (where u fill in the info) when clicked)
         newGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startGameSetup();
             }
         });
+        final ImageView ferretIcon = (ImageView) findViewById(R.id.ferretIcon);
+        ferretIcon.setImageResource(R.drawable.iconanimation);
+        final AnimationDrawable iconanimation = (AnimationDrawable)ferretIcon.getDrawable();
+        iconanimation.start();
     }
 
     public void startGameSetup() {
